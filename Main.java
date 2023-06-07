8a15aeb075f793c65f6a952d1d71b25ac14fbe0d
Anthony J Souza <ajsouza88@gmail.com>
2023-06-07 14:57:45 -0700

    completed a1 for 413

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        while(true){
+        for(;;){
             System.out.println("hello you");
         }
     }
--- main.c
+++ main.c
@@ -2,13 +2,8 @@
 
 int
 main(void){
-<<<<<<< HEAD
     while(1){
         printf("hello you\n");
-=======
-    for(;;){
-        printf("hello me\n");
->>>>>>> a44df0629892958c31f552adf1533d32cc0c7b5e
     }
     return 0;
 }

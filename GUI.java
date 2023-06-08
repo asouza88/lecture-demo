5f804c2c6d378ca328cfd1e73cb5c199a3a4b4df
Anthony J Souza <ajsouza88@gmail.com>
2023-06-07 18:42:44 -0700

    please have conflict

--- GUI.java
+++ GUI.java
@@ -1,8 +1,8 @@
 public class GUI {
     public static void main(String[] args) {
         GUI g = new GUI();
-        for (String string : args) {
-            System.out.println(args);
+        for(;;){
+            System.out.println("hello minions");
         }
     }
 }
